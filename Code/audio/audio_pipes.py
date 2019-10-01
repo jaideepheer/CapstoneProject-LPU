2aa44d729fbb8e3101c7a058f79b5427a72c3974
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2019-10-01 10:27:23 +0530

    Added AudioToTextDeepSpeechPipe using Mozilla's DeepSpeech

--- Code/audio/audio_pipes.py
+++ Code/audio/audio_pipes.py
@@ -1,6 +1,10 @@
 from pyaudio import PyAudio
 from pipedefs.pipe import PushPipe
 from utils.typedefs import AudioRecording
+from speech_recognition import AudioData, Recognizer, UnknownValueError
+import numpy as np
+from dataclasses import dataclass
+import deepspeech
 
 class AudioPlaybackPipe(PushPipe[AudioRecording, AudioRecording]):
     def __init__(self, postProcessCallback=None):
@@ -19,7 +23,35 @@ class AudioPlaybackPipe(PushPipe[AudioRecording, AudioRecording]):
             self.stream.close()
         self.pyaudio.terminate()
 
-from speech_recognition import AudioData, Recognizer, UnknownValueError
+class AudioToTextDeepSpeechPipe(PushPipe[AudioRecording, str]):
+    @dataclass
+    class LanguageModel_Config:
+        lm_path: str
+        trie_path: str
+        # The alpha hyperparameter of the CTC decoder. Language Model weight
+        LM_WEIGHT: float = 1.5
+        # The beta hyperparameter of the CTC decoder. Word insertion bonus.
+        VALID_WORD_COUNT_WEIGHT: float = 2.25
+    def __init__(self, model_pb_path: str, alphabet_path: str, beam_width: int = 512, language_model: LanguageModel_Config = None,  postProcessCallback=None):
+        super().__init__(postProcessCallback=postProcessCallback)
+        self.model = deepspeech.Model(model_pb_path, alphabet_path, beam_width)
+        if(language_model is not None):
+            self.model.enableDecoderWithLM(language_model.lm_path, language_model.trie_path, language_model.LM_WEIGHT, language_model.VALID_WORD_COUNT_WEIGHT)
+        self.stream_state = None
+        self.beam_width = beam_width
+    def process(self, recording: AudioRecording, passThrough: PushPipe.PassThrough):
+        self.stream_state = self.model.createStream(recording.METADATA.Rate)
+        data = np.frombuffer(recording.data, np.int16)
+        padding = (self.beam_width - (data.shape[0]%self.beam_width))%self.beam_width
+        data = np.pad(data, (0, padding), 'constant')
+        print("Total:",data.shape)
+        data = np.split(data, self.beam_width)
+        print('Data:',len(data),data[0].shape,data[-1].shape)
+        # feed samples to stream
+        for frame in data:
+            self.model.feedAudioContent(self.stream_state, frame)
+        return self.model.finishStream(self.stream_state)
+
 class AudioToTextSphinxPipe(PushPipe[AudioRecording,str]):
     def __init__(self, postProcessCallback=None):
         super().__init__(postProcessCallback=postProcessCallback)
--- Code/live_pipe_test.py
+++ Code/live_pipe_test.py
@@ -2,7 +2,7 @@ from pipedefs.on_demand_pipes import OnDemandPhotoClickPipe, OnDemandAudioRecord
 from utils.typedefs import AudioMetadata, PyAudioFormat_Type
 from sys import stdout
 from pipedefs.pipe_utils import PipeLineProfilingWrapper
-from audio.audio_pipes import AudioPlaybackPipe, AudioToTextSphinxPipe
+from audio.audio_pipes import AudioPlaybackPipe, AudioToTextSphinxPipe, AudioToTextDeepSpeechPipe
 from pipedefs.pipe import PushPipe
 from cv2 import waitKey, destroyAllWindows, imshow, destroyWindow, cvtColor, COLOR_RGB2BGR
 import functools
@@ -17,13 +17,18 @@ def showImage_internal(winName, result, image, error, passThrough: PushPipe.Pass
 def showImage(winName):
     return functools.partial(showImage_internal, winName)
 
-if(True):
+if(False):
     from emotion.emotion_detect_pipes import EmotionExtractorOArriagaPipe
     from face.face_detect_pipes import FaceExtractorMTCNNPipe, FaceExtractorDNNPipe
     pipeline = OnDemandPhotoClickPipe(postProcessCallback=showImage("clicker_callback")).connect(
         FaceExtractorDNNPipe(postProcessCallback=showImage("face_DNN_callback")).connect(
             #EmotionExtractorOArriagaPipe()
         )
+    )
+elif(True):
+    pipeline = OnDemandAudioRecordPipe(AudioMetadata(),postProcessCallback=None).connect(
+        AudioPlaybackPipe(),
+        AudioToTextDeepSpeechPipe("C:/Users/hp/Desktop/deepspeech/mozDS/deepspeech-0.5.1-models/deepspeech-0.5.1-models/output_graph.pb","C:/Users/hp/Desktop/deepspeech/mozDS/deepspeech-0.5.1-models/deepspeech-0.5.1-models/alphabet.txt", language_model=AudioToTextDeepSpeechPipe.LanguageModel_Config("C:/Users/hp/Desktop/deepspeech/mozDS/deepspeech-0.5.1-models/deepspeech-0.5.1-models/lm.binary","C:/Users/hp/Desktop/deepspeech/mozDS/deepspeech-0.5.1-models/deepspeech-0.5.1-models/trie"))
     )
 else:
     pipeline = OnDemandAudioRecordPipe(AudioMetadata(),postProcessCallback=None).connect(
@@ -39,7 +44,7 @@ while(True):
     print("Loop Time: %f [%d FPS]"%(loopTime,1//loopTime))
     loopTime = time.time()
     result: PushPipe.PassThrough = pipeline.push(None, PushPipe.PassThrough())
-    #print(result.getExtrasHistory()[-1]['Process_Output'])
+    print(result.getExtrasHistory()[-1]['Process_Output'])
     print('====================================')
     if waitKey(1) & 0xFF == ord('q'):
         break
